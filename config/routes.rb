27e16c7a3dfa9a1f8c9e64d6e7006c7184ee8163
puppypaws <marcusjoshualee@gmail.com>
2019-09-20 01:01:53 +0800

    Routes for Team and Player

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
+
+  # Devise (home)
   devise_for :users
   get 'home/index'
-  
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Teams
+  get '/teams' => 'teams#index', as: 'teams'
+  get '/teams/new' => 'teams#new', as: 'new_team'
+  post '/teams' => 'teams#create'
+  get '/teams/:id' => 'teams#show', as: 'team'
+  get '/teams/:id/edit' => 'teams#edit', as: 'edit_team'
+  patch '/teams/:id' => 'teams#update'
+  delete '/teams/:id' => 'teams#destroy'
+
+  # Players
+  get '/players' => 'players#index', as: 'players'
+  get '/players/new' => 'players#new', as: 'new_player'
+  post '/players' => 'players#create'
+  get '/players/:id' => 'players#show', as: 'player'
+  get '/players/:id/edit' => 'players#edit', as: 'edit_player'
+  patch '/players/:id' => 'players#update'
+  delete '/players/:id' => 'players#destroy'
+
+  # Teams & Players association
+  get '/teams/:team_id/players' => 'players#index', as: 'team_players'
+  get '/teams/:team_id/players/new' => 'players#create', as: 'new_team_player'
+  post '/parks/:park_id/rangers' => 'rangers#create'
+  
 end
